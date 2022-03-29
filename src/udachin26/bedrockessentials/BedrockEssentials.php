39a919c11e11ac8a638c70178cfddd6b39ae8809
Max <udachin26@mail.ru>
2022-03-29 07:00:24 +0300

    dev next

--- src/udachin26/bedrockessentials/BedrockEssentials.php
+++ src/udachin26/bedrockessentials/BedrockEssentials.php
@@ -6,6 +6,7 @@ namespace udachin26\bedrockessentials;
 
 use pocketmine\plugin\PluginBase;
 use udachin26\bedrockessentials\command\CommandManager;
+use udachin26\bedrockessentials\language\LanguageManager;
 use udachin26\bedrockessentials\listener\SessionListener;
 use udachin26\bedrockessentials\provider\ProviderManager;
 use udachin26\bedrockessentials\session\SessionManager;
@@ -15,12 +16,14 @@ final class BedrockEssentials extends PluginBase{
     private ProviderManager $providerManager;
     private CommandManager $commandManager;
     private SessionManager $sessionManager;
+    private LanguageManager $languageManager;
     
 
     public function onEnable(): void{
         $this->providerManager = new ProviderManager($this);
         $this->commandManager = new CommandManager($this);   
         $this->sessionManager = new SessionManager($this);
+        $this->languageManager = new LanguageManager($this);
 
         $this->getServer()->getPluginManager()->registerEvents(new SessionListener($this), $this);
     }
@@ -35,5 +38,9 @@ final class BedrockEssentials extends PluginBase{
 
     public function getSessionManager(): SessionManager{
         return $this->sessionManager;
+    }
+
+    public function getLanguageManager(): LanguageManager{
+        return $this->languageManager;
     }
 }
